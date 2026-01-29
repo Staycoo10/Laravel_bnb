afd83a13b9a653ea3efc6681d7ffca3f9504c192
Botnari Bogdan <141923746+Staycoo10@users.noreply.github.com>
2026-01-29 13:36:09 +0200

    Add About and Contact routes
    Introduced new '/about' and '/contact' routes that return simple string responses for each respective page.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Ruta About
+Route::get('/about', function () {
+    return "Pagina About";
+});
+
+// Ruta Contact
+Route::get('/contact', function () {
+    return "Pagina Contact";
+});
